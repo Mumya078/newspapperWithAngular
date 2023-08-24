053463d1b81982869aca5521e569755a249f0252
Yusuf Emir Tatlı <yusufemirtatli96@gmail.com>
2023-08-24 17:43:20 +0300

    Newspaper projesi başlangıcı. Topbar ve Slider oluşturuldu.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { TopbarComponent } from './topbar/topbar.component';
+import { SliderComponent } from './slider/slider.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    TopbarComponent,
+    SliderComponent
   ],
   imports: [
     BrowserModule
